b25971365e55320568e2b7495d0f2ca42c582c00
Maksim99745 <maksimmuhametzyanov11@gmail.com>
2023-12-25 19:11:59 +0100

    feat: add more tasks

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,15 +20,100 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  // encrypt() {
+
+  // }
+  // decrypt() {
+  //   throw new NotImplementedError('Not implemented');
+  //   // remove line with error and write your code here
+  // }
+    constructor(direction) {
+      this.direction = direction !== undefined ? direction : true;
+    }
+  
+    checkError(message, key) {
+      if (message === undefined  || key === undefined) {
+        throw new Error("Incorrect arguments!");
+      }
+    }
+  
+    getCharCode(letter) {
+      if (letter === " ") {
+        return " ";
+      }
+      return letter.toUpperCase().charCodeAt(0) - "A".charCodeAt(0);
+    }
+  
+    getLetterFromCharCode(charCode) {
+      if (charCode === " ") {
+        return " ";
+      }
+      return String.fromCharCode(charCode + "A".charCodeAt(0));
+    }
+  
+    isSymbol(char) {
+      return char.toLowerCase() === char.toUpperCase()  || char === " ";
+    }
+  
+    encrypt(message, key) {
+      this.checkError(message, key);
+  
+      const messageArray = message.toUpperCase().split("");
+      const keyArray = key.toUpperCase().split("");
+  
+      let keyIndex = 0;
+  
+      const encryptedMessage = messageArray.map((letter) => {
+        if (this.isSymbol(letter)) {
+          return letter;
+        }
+  
+        const messageCharCode = this.getCharCode(letter);
+        const keyCharCode = this.getCharCode(
+          keyArray[keyIndex % keyArray.length]
+        );
+  
+        let encryptedCharCode = (messageCharCode + keyCharCode) % 26;
+  
+        keyIndex++;
+        return this.getLetterFromCharCode(encryptedCharCode);
+      });
+  
+      return this.direction
+        ? encryptedMessage.join("")
+        : encryptedMessage.reverse().join("");
+    }
+  
+    decrypt(message, key) {
+      this.checkError(message, key);
+  
+      const messageArray = message.toUpperCase().split("");
+      const keyArray = key.toUpperCase().split("");
+  
+      let keyIndex = 0;
+  
+      const decryptedMessage = messageArray.map((letter) => {
+        if (this.isSymbol(letter)) {
+          return letter;
+        }
+  
+        const messageCharCode = this.getCharCode(letter);
+        const keyCharCode = this.getCharCode(
+          keyArray[keyIndex % keyArray.length]
+        );
+  
+  
+        let decryptedCharCode = (messageCharCode + 26 - keyCharCode) % 26;
+  
+        keyIndex++;
+        return this.getLetterFromCharCode(decryptedCharCode);
+      });
+  
+      return this.direction
+        ? decryptedMessage.join("")
+        : decryptedMessage.reverse().join("");
+    }
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-}
 
 module.exports = {
   VigenereCipheringMachine
